6b709bdc62d99b6b13b923a6f12e39d89af1cb1e
dyr4 <118971998+dyr4@users.noreply.github.com>
2024-10-29 22:26:26 +0500

    3.2.2

---
+++ 3.3.2/main.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+// Функция для генерации ключа
+string generateKey(const string& key, size_t length) {
+    string generatedKey;
+    for (size_t i = 0; i < length; ++i) {
+        generatedKey += key[i % key.size()]; //Генерируем ключ нужный длины
+    }
+    return generatedKey;
+}
+
+// Функция для шифрования и дешифрования
+string xorCipher(const string& data, const string& key) {
+    string result = data;
+    for (size_t i = 0; i < data.size(); ++i) {
+        result[i] = data[i] ^ key[i]; // Используем XoR для шифрования и дешифрования
+    }
+    return result;
+}
+
+// Функция для определения гаммы
+string chooseGammaType(const string& text) {
+    int latinCount = 0;
+    int cirillicCount = 0;
+
+    for (char c : text) {
+        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) { // Подсчёт латинских символов
+            latinCount++;
+        } else if ((c >= 'А' && c <= 'Я') || (c >= 'а' && c <= 'я')) { // Подсчёт кириллицы
+            cirillicCount++;
+        }
+    }
+    if(latinCount > cirillicCount)
+    {
+        return "latin";
+    }
+    else
+    {
+        return "cirillic";
+    }
+}
+
+
+int main() {
+    setlocale(0, "Russian");
+
+    string file_in, file_out;
+
+    file_in = "file_in.txt";
+    file_out = "file_out.txt";
+
+    // Чтение текста из файла
+    string text;
+    ifstream fileIn(file_in);
+
+    if (fileIn.is_open()) {
+        getline(fileIn, text);
+        fileIn.close();
+    }
+    else
+    {
+        cout << "Ошибка: Не удалось открыть файл " << file_in << endl;
+    }
+
+    string gammaType = chooseGammaType(text);
+
+    // Выбор гаммы
+    string gamma;
+    if (gammaType == "latin") {
+        gamma = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; // Латинская гамма
+    } else {
+        gamma = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя"; // Кириллическая гамма
+    }
+
+    // Генерация ключа нужной длины
+    string generatedKey = generateKey(gamma, text.size()); // Используем введенный ключ
+    cout << "Генерация ключа: " << generatedKey << endl;
+
+    // Шифрование
+    string ciphertext = xorCipher(text, generatedKey);
+    cout << "Зашифрованный текст: " << ciphertext << endl;
+
+    // Добавление байта с длиной открытого текста
+    char lengthByte = static_cast<char>(text.size());
+    ciphertext += lengthByte; //  Добавляем байт длины
+
+    // Запись зашифрованного текста в файл
+    ofstream outputFile(file_out);
+    if (!outputFile.is_open()) {
+        cout << "Ошибка при открытии файла " << file_out << endl;
+        return 1;
+    }
+
+    outputFile << ciphertext;
+    outputFile.close();
+
+    // Дешифрование текста
+    int originalLength = static_cast<unsigned char>(ciphertext.back()); // Получаем длину из последнего байта
+    string decryptedtext = xorCipher(ciphertext.substr(0, ciphertext.size() - 1), generatedKey); // Убираем последний байт
+    cout << "Расшифрованный текст: " << decryptedtext << endl;
+
+    return 0;
+}
