3736ea2294c3c81db37b64b4a617c4da8a6d865c
dyr4 <118971998+dyr4@users.noreply.github.com>
2024-11-02 17:34:00 +0500

    3.4.1

---
+++ 3.4.1/main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+// Функция для генерации ключа
+string generateKey(const string& key, size_t length) {
+    string generatedKey;
+    for (size_t i = 0; i < length; ++i) {
+        generatedKey += key[i % key.size()]; // Генерируем ключ нужной длины
+    }
+    return generatedKey;
+}
+
+// Функция для шифрования и дешифрования
+string xorCipher(const string& data, const string& key) {
+    string result = data;
+    for (size_t i = 0; i < data.size(); ++i) {
+        result[i] = data[i] ^ key[i]; // Используем XOR для шифрования и дешифрования
+    }
+    return result;
+}
+
+// Подсчёт контрольной суммы
+char kontrolnayaSumma(const string& text)
+{
+    int checksum = 0;
+    for (int i = 0; i < text.length(); i++) {
+        checksum += (int)text[i];
+    }
+    return static_cast<char>(checksum);
+}
+
+int main() {
+    setlocale(0, "Russian");
+
+    string file_in, file_out;
+    file_in = "file_in.txt";
+    file_out = "file_out.txt";
+
+    // Чтение текста из файла
+    string text;
+    ifstream fileIn(file_in);
+
+    if (fileIn.is_open()) {
+        getline(fileIn, text);
+        fileIn.close();
+    } else {
+        cout << "Ошибка: Не удалось открыть файл " << file_in << endl;
+        return 1; // Завершаем программу при ошибке
+    }
+
+    // Выбор гаммы
+    string gamma = "2";
+
+    // Генерация ключа нужной длины
+    string generatedKey = generateKey(gamma, text.size());
+    cout << "Генерация ключа: " << generatedKey << endl;
+
+    // Подсчёт контрольной суммы
+    char kontSum = kontrolnayaSumma(text);
+    text += kontSum;
+
+    // Шифрование
+    string ciphertext = xorCipher(text, generatedKey);
+    cout << "Зашифрованный текст: " << ciphertext << endl;
+
+    // Запись зашифрованного текста в файл
+    ofstream outputFile(file_out);
+    if (!outputFile.is_open()) {
+        cout << "Ошибка при открытии файла " << file_out << endl;
+        return 1;
+    }
+
+    outputFile.write(ciphertext.c_str(), ciphertext.size());
+    outputFile.close();
+
+    // Дешифрование текста
+    string decryptedtext = xorCipher(ciphertext, generatedKey);
+    cout << "Расшифрованный текст: " << decryptedtext<< endl;
+
+     // Извлечение контрольной суммы
+    char kontAfterSum = decryptedtext.back();
+    decryptedtext.pop_back();
+
+    // Проверка контрольной суммы
+    kontSum = kontrolnayaSumma(decryptedtext);
+    if (kontSum == kontAfterSum) {
+        cout << "Контрольная сумма совпадает. Дешифрование успешно!" << endl;
+    } else {
+        cout << "Контрольная сумма не совпадает." << endl;
+    }
+
+    return 0;
+}
